30cdd7c151ae5564fb218b28e1048f9eecb00502
Md. Anisur Rahman Likhon <47313052+anisurrahmanlikhon@users.noreply.github.com>
2023-03-11 09:03:16 +0600

    Create Operators, Binary.c

---
+++ Operators, Binary.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+  printf("add %i\n", 10 + 2);
+  printf("sub %i\n", 13 - 1);
+  printf("mul %i\n", 3 * 4);
+  printf("div %i\n", 72 / 6);
+  printf("rem %i\n", 32 % 20);
+  printf("and %i\n", 15 & 12);
+  printf("or  %i\n", 4 | 8);
+  printf("xor %i\n", 6 ^ 10);
+  printf("shl %i\n", 3 << 2);
+  printf("shr %i\n", 96 >> 3);
+  printf("comma %i\n", (4, 12));
+}
